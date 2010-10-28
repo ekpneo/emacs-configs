e847a3391c6219b7e7297db186c00d2bb48b06f1
Ted Morse <tmorse@gmail.com>
2010-10-27 22:18:06 -0700

    start of the chain... and customizations

---
+++ emacs.el
@@ -0,0 +1,19 @@
+(add-to-list 'load-path "~/.emacs.d")
+(load "local")
+
+;; Below this line is emacs-land
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(menu-bar-mode t)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#141414" :foreground "#F8F8F8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
