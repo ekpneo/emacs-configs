237797b4d4cbd6d32f7ff1dfcd4bab734e9b3f19
Ted Morse <tmorse@gmail.com>
2010-10-27 22:23:49 -0700

    misc options

---
+++ ekpneo/global.el
@@ -0,0 +1,14 @@
+
+(setq-default fill-column 79)
+
+; Indentation
+(setq-default tab-width 2)
+(setq-default c-basic-offset 2)
+(setq-default python-indent 2)
+
+; Spaces, not tabs
+(setq-default indent-tabs-mode nil)
+
+(prefer-coding-system 'utf-8)
+
+(cua-mode nil)
