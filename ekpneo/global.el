8ee4d64edc0292c1ff7d1fd6c9f320b388308eab
Ted Morse <tmorse@gmail.com>
2010-12-18 19:37:04 -0800

    don't litter backup files

--- ekpneo/global.el
+++ ekpneo/global.el
@@ -11,4 +11,11 @@
 
 (prefer-coding-system 'utf-8)
 
+; I hate CUA mode
 (cua-mode nil)
+
+; Don't litter ~ files
+(setq backup-directory-alist
+      `(,(cons "." (concat user-emacs-directory "auto-backup-files/"))))
+(setq auto-save-file-name-transforms
+      `((".*" ,(concat user-emacs-directory "auto-save-files/") t)))
