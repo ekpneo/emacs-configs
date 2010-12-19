01fae8c3ea154f725978f935f56c4f0d5aaf20d2
Ted Morse <tmorse@gmail.com>
2010-12-18 19:35:17 -0800

    moved fonts to .Xdefaults

--- emacs.el
+++ emacs.el
@@ -11,9 +11,3 @@
  '(menu-bar-mode t)
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#141414" :foreground "#F8F8F8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
